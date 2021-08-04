25ceff85974edd5bc46a436bdf9b6adb049fe116
Gustavo Ramos <gustavoramosrock@gmail.com>
2021-08-04 02:38:28 -0300

    adicionando aulas

---
+++ Preparando Astronauta - DOM/Manipulando Conteudo/textContent/textcontent.js
@@ -0,0 +1,10 @@
+// Manipulando conteudo
+// textContent
+
+const element = document.querySelector('h1')
+
+element.textContent = "Hello World"
+
+// altera o conteudo de texto do elemento
+
+console.log(element.textContent)
